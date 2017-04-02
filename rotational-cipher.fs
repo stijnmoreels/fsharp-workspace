73251a6fbaf7c08828b02708fe527e6ab5c06d34
Stijn Moreels <stijn.moreels@codit.eu>
2017-04-02 15:08:39 +0200

    Format rational cipher

--- rotational-cipher.fs
+++ rotational-cipher.fs
@@ -1,12 +1,15 @@
 let shiftChar (c:char) (s:int) =
     let max = int 'z'
     let min = int 'a'
-
     let increasedChar = (int c) + s
-    char (if increasedChar > max then min + (increasedChar - max) else increasedChar)
+    
+    char (if increasedChar > max 
+            then min + (increasedChar - max) 
+            else increasedChar)
 
 let rotationalCipher (key:int) (value:string) =
-    System.String.Concat([for c in value -> (shiftChar c key)])
+    System.String.Concat(
+        [for c in value -> (shiftChar c key)])
     
 
 rotationalCipher 13 "abcdefghijklmnopqrstuvwxyz"
